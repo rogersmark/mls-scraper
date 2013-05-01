04d978406f094ef38b2bfdb76cfd5d072ebd4571
Dan Getelman <dgetelman@gmail.com>
2013-05-01 13:32:55 -0700

    Two player parsing bug fixes
    The first was an issue caused by assigning a mutable empty list as
    a default value for a new team. It might've been limited to the tests,
    but still kinda nasty.
    
    The second was that away subs were being added as keepers instead of subs.

--- mls_scraper/mls_scraper.py
+++ mls_scraper/mls_scraper.py
@@ -16,11 +16,22 @@ class Team(object):
     keepers = []
     stats = {}
 
-    def __init__(self, name=None, players=[], keepers=[], stats={}):
+    def __init__(self, name=None, players=None, keepers=None, stats=None):
         self.name = name
-        self.players = players
-        self.keepers = keepers
-        self.stats = stats
+        if self.players:
+            self.players = players
+        else:
+            self.players = []
+
+        if self.keepers:
+            self.keepers = keepers
+        else:
+            self.keepers = []
+
+        if self.stats:
+            self.stats = stats
+        else:
+            self.stats = {}
 
     def __unicode__(self):
         return u'%s' % self.name
@@ -227,8 +238,9 @@ class GameStatSet(object):
 
         home_subs = self._parse_stat_table(home_table)
         away_subs = self._parse_stat_table(away_table)
+
         self.home_team.players.extend(home_subs)
-        self.away_team.keepers.extend(away_subs)
+        self.away_team.players.extend(away_subs)
 
     def _process_goals(self):
         ''' Process the Goals div which has an id of "goals" '''
