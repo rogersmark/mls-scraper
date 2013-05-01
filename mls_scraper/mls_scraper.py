e3b6219a3ea1d36110a382ed02fd3137bba8e1b8
Dan Getelman <dgetelman@gmail.com>
2013-05-01 13:35:26 -0700

    Whitespace changes to make it pep8 compliant

--- mls_scraper/mls_scraper.py
+++ mls_scraper/mls_scraper.py
@@ -58,7 +58,7 @@ class GameStatSet(object):
         'HOU': 'Houston Dynamo',
         'LA': 'LA Galaxy',
         'MON': 'Montreal Impact',
-        'MTL': 'Montreal Impact', # Sometimes its MON, others its MTL
+        'MTL': 'Montreal Impact',  # Sometimes its MON, others its MTL
         'NY': 'New York Red Bulls',
         'NE': 'New England Revolution',
         'PHI': 'Philadelphia Union',
@@ -67,7 +67,7 @@ class GameStatSet(object):
         'SEA': 'Seattle Sounders FC',
         'SJ': 'San Jose Earthquakes',
         'SKC': 'Sporting Kansas City',
-        'KC': 'Sporting Kansas City', # Sometimes its KC, others its SKC
+        'KC': 'Sporting Kansas City',  # Sometimes its KC, others its SKC
         'TOR': 'Toronto FC',
         'VAN': 'Vancouver Whitecaps FC',
     }
